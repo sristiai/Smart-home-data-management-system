77b32f6901d1a7dab7b8cef50e228d1da9c2316c
sristiai <sristibhadani28@gmail.com>
2025-03-06 13:34:32 +0100

    Add files via upload

---
+++ src/model/__init__.py
@@ -0,0 +1,9 @@
+# src/models/__init__.py
+from src.models.user_model import User
+from src.models.houses_model import House
+from src.models.residents_model import Resident
+from src.models.calendars_model import Calendar
+from src.models.calendar_events_model import CalendarEvent
+from src.models.calendar_events_permissions_model import CalendarEventPermission
+from src.models.resident_preferences_model import ResidentPreference
+from src.models.notification_logs_model import NotificationLog
